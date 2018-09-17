c20b945fce948a73cd462d909f9d67412b01cd31
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-17 21:37:04 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,6 +37,7 @@ def until_iterator(number_of_times)
     phrase = "Welcome to Flatiron School's Web Development Course!"
     until counter == number_of_times
     puts phrase
+    counter += 1
   end
 end
 
